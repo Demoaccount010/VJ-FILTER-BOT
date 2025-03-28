83f86d2753068aac7e63d863bcd23ba1c1d94bc7
Demoaccount010 <rdx27652@gmail.com>
2025-03-28 18:10:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/smilesupportgroup')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/crunchyroll_hindi_dub_yt')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+PDHhbic78IllZGRl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'smilesupportgroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/yoursmileyt')
 
